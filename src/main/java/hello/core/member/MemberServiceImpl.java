377009ad793995d8c5196986db0a022a6a77ddc8
이상훈 <tkdgns25300@gmail.com>
2024-12-20 16:54:51 +0900

    component scan

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,20 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Component
+@Controller
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
+
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-    //테스트 용도
+//    테스트 용도
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
